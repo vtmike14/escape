a5c43ffa09002658550d3ebfe77284fae5723d3f
Caleb Stroud <calebs@vt.edu>
2014-05-13 12:40:52 -0400

    Fixed look command for when the room has a lock and the person has the item to unlock (go worked fine) Also added features in response to reviews: 	Added two spaces before "<<" to create a better, clearere interface 	Changed intro text to correctly display look commands (especially looking at inventory) 	Added comments

--- escape.py
+++ escape.py
@@ -4,6 +4,7 @@ import house
 import yaml
 import sys
 
+# Read in graphics and print map
 f = open('intro.txt', 'r')
 title = f.read()
 f.close()
@@ -13,24 +14,28 @@ map = f.read()
 f.close()
 print(map)
 print("                       TYPE 'start' TO BEGIN...")
-line = input(">>")
+line = input("  >>")
 while line != 'start':
 	print("Invalid input: please type 'start' to begin...")
-	line = input(">>")
+	line = input("  >>")
 
-intro = "\n\nWelcome to the game ESCAPE! where you are stuck\nin a house until you can figure out what to do to get out.\n\nIn order to get out, you must collect the proper items\nas well as find the correct rooms to go through to get to your freedom,\nand if you don't make it through, well, it just sucks to suck.\n\nThe commands for the game are:\ngo [direction]\nlook [direction]\npickup [item]\nfight [enemy]\n\nFor those of you with weak constitutions and can't make it through\nthe game, well, you can type ""exit"" in order to get out of the game.\n\nGood Luck...\n\n"
+# Print directions
+intro = "\n\nWelcome to the game ESCAPE! where you are stuck\nin a house until you can figure out what to do to get out.\n\nIn order to get out, you must collect the proper items\nas well as find the correct rooms to go through to get to your freedom,\nand if you don't make it through, well, it just sucks to suck.\n\nThe commands for the game are:\ngo [direction]\nlook\nlook items\nlook [direction]\npickup [item]\n\nFor those of you with weak constitutions and can't make it through\nthe game, well, you can type ""exit"" in order to get out of the game.\n\nGood Luck...\n\n"
 
 print(intro)
 
+# Load data
 data = yaml.load(open('house.yaml', 'r'))
 
 myhouse = house.House(data, "LOBBY")
 
 print(myhouse.rooms["LOBBY"])
-#print(myhouse)
 
+# Play game
 while True:
-	line = input(">>")
+	# Define end var so that a NameError is not received if an invalid command is giver first time through while loop
+	end = ''
+	line = input("  >>")
 	if line == 'exit':
 		break	
 	action, argument = line.partition(' ')[::2]
@@ -40,6 +45,3 @@ while True:
 		print("{0} is an invalid command...".format(action))	
 	if end == 'endgame':
 		break
-
-
-
--- house.py
+++ house.py
@@ -1,3 +1,4 @@
+# Holds items in a room. A person also has a list of items
 class Item:	
 	def __init__(self, data):
 		self.item = data
@@ -8,38 +9,45 @@ class Item:
 	def __eq__(self, other):
 		return self.item == other.item
 
+# Defines a room of a house
 class Room:
 	"""A single room"""
 
+	# Initializer. Creates vars: var with name, var with description, list with items, dict with directions,
+	# dict with lockes rooms and their unlocking item, and dict with enemy and item to defeat it
 	def __init__(self, data, name):
 		self.name = name
 		self.description = data['desc']
 		self._items = []
 		for item in data['items']:
 			self._items.append(Item(item))
+		# Create dict of dirs with all key valyes that are not in otherthan_dir
 		self.dirs = {}
 		keys_otherthan_dir = ['items', 'desc', 'enemy', 'lock']
 		for key in data:
 			if key not in keys_otherthan_dir:
 				self.dirs[key] = data[key]
-		#locked room
+		# Locked room
 		self.locks = {}
 		if 'lock' in data:
 			for key,value in data['lock'].items():
 				self.locks[key] = value
-
+		# Enemy, if exists
 		self.enemies = {}
 		if 'enemy' in data:
 			for key,value in data['enemy'].items():
 				self.enemies[key] = value
 
+	# Checks to see if there is a lock on the item, Anything can be locked but this is usually a direction
+	# Parameter
+	#	item	checks if item has a lock
 	def isLocked(self, item):
-		#print(item + " lock:: " + " ".join(name + " : " + value for name,value in self.locks.items()) + "is " + str(isinstance(item, Room)))
 		if item in self.locks:
 			return True
 		else:
 			return False
 
+	# Returns true if this room has an enemy and false if not
 	def hasEnemy(self, item):
 		if item in self.enemies:
 			return True
@@ -53,6 +61,7 @@ class Room:
 			str_items = [str(item) for item in self._items]
 			return self.name + "\n" + self.description + "\nitems: " + ", ".join(str_items) + "\n" + "\n".join([dir+": "+str(room) for dir,room in self.dirs.items()])
 
+# Defines a person in a house. This is the user
 class Person:
 	"""Character playing the game"""
 	
@@ -64,48 +73,54 @@ class Person:
 		if not str_items:
 			return "You do not have any items."
 		else:
-			return "You have these items: " + ", ".join(str_items)
+			return "You have these item(s): " + ", ".join(str_items)
 		return str_items
 
+# Defines a house with rooms and a person
 class House:
-	"""A house contains one or more rooms"""
+	"""A house contains one or more rooms and a person"""
 
+	# Initializer. Creates a dict of rooms, var with current room name, and person within the house
 	def __init__(self, data, current_name):
 		self.rooms = {}
 		for key,value in data.items():
 			self.rooms[key] = Room(data[key], key)
-		#self._current = self.rooms[current_name]
 		self._current_name = current_name
 		self.scared_person = Person()
 
+	# Defines the movement of the person throughout the house
+	# Parameter:
+	#	direction	the direction to move. If there is no room in that direction, a statement is printed out
 	def go(self, direction):
 		cur_room = self.rooms[self._current_name]
 		if direction in cur_room.dirs:
+			# Room is locked and there is no enemy
 			if not cur_room.isLocked(cur_room.dirs[direction]) and not cur_room.hasEnemy(cur_room.dirs[direction]):
 				self._current_name = cur_room.dirs[direction]
 				print(self.current)
-
+			# Room is locked but person has item
 			elif cur_room.isLocked(cur_room.dirs[direction]) and (cur_room.locks[cur_room.dirs[direction]] in self.scared_person._citems):
 				self._current_name = cur_room.dirs[direction]
 				print(self.current)
-			
+			# Room has an enemey and person has item to aly enemy
 			elif cur_room.hasEnemy(cur_room.dirs[direction]) and (cur_room.enemies[cur_room.dirs[direction]] in self.scared_person._citems):
 				print("Congratulations!!! You have slain the nasty rat with the machette and escaped the scary house!")
 				print("Please stay tuned for more updates from the amazing escape game development team presented to you by:")
 				print("Neil Singh, Caleb Stroud, Michael Burton, and Timothy Hogarty")
 				return "endgame"			
-
+			# Room has enemy but person does not have required item
 			elif cur_room.hasEnemy(cur_room.dirs[direction]):
 				print("You need to defeat the enemy before you can go outside.")
 				print("You will need a blade of some kind to defeat the monster.")
-	
+			# Room is locked
 			else:
 				print("You need a " + cur_room.locks[cur_room.dirs[direction]] + " to access this direction " + direction + ".") 
-			#self._current = self.rooms[self._current_name]
-			#print(self._current)
 		else:
 			print("There is no room in the direction " + direction + ".")
 
+	# Command to pickup and item in a room. If the item is not in hte room, prints out a message
+	# Parameter
+	#	item	item to be picked up
 	def pickup(self, item):
 		if Item(item) in self.rooms[self._current_name]._items:
 			self.scared_person._citems.append(item)
@@ -114,6 +129,12 @@ class House:
 		else:
 			print("Unable to pickup. " + item + " is not in the " + self._current_name + ".")
 
+	# Command to look at certain things. There are three forms:
+	#	look			descrip of current room
+	#	look items		list of person's items
+	#	look [direction]	descript of room in dir
+	# Paremeter
+	#	object	ojbect to be parsed
 	def look(self, object):
 		cur = self.rooms[self._current_name]
 
@@ -124,12 +145,21 @@ class House:
 		elif object not in cur.dirs:
 			print("There is not a room in the chosen direction.") 
 		elif cur.isLocked(cur.dirs[object]):
-			print("You cannot look in this direction.")
+			if self.canAccess(object):
+				print(self.rooms[self.rooms[self._current_name].dirs[object]])
+			else:
+				print("You cannot look in this direction.")
 		elif object in cur.dirs:
 			name_in_dir = self.rooms[self._current_name].dirs[object]
 			print(self.rooms[name_in_dir])
 		else:
 			print("Not a valid look command.")
+
+	# Returns true if there is a lock and the person has the item
+	def canAccess(self, direction):
+		cur_room = self.rooms[self._current_name]
+		return (cur_room.isLocked(cur_room.dirs[direction]) and (cur_room.locks[cur_room.dirs[direction]] in self.scared_person._citems))
+		
 
 	@property
 	def current(self):
@@ -137,5 +167,3 @@ class House:
 
 	def __str__(self):
 		return "World:\n{0}".format("\n%%\n".join([name+": "+str(room) for name,room in self.rooms.items()]))
-
-
